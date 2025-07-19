795cf233753df7ee77be4fe591c01199878f9169
nikhilbuilds <devnikhil0306@gmail.com>
2025-07-19 08:15:09 +0530

    linting issues fixes

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,13 +1,21 @@
 import { workedOnItems } from "@/app/data/projects";
 import { notFound } from "next/navigation";
 import ProjectClientPage from "./client-page";
+import { use } from "react";
 
-export default function ProjectPage({ params }: { params: { id: string } }) {
-  if (!params.id) {
+export default function ProjectPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const resolvedParams = use(params);
+
+  if (!resolvedParams.id) {
     notFound();
   }
+
   const project =
-    workedOnItems.find((p) => p.id === parseInt(params.id, 10)) || null;
+    workedOnItems.find((p) => p.id === parseInt(resolvedParams.id, 10)) || null;
 
   if (!project) {
     notFound();
